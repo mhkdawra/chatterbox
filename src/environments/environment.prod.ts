92ecf2c6926bf9ba338d67f0e90547838d10ad97
mhkdawra <mhkdawra@gmail.com>
2018-10-23 12:26:39 +0530

    Added environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "firebase_api_key",
+    authDomain: "firebase_authDomain",
+    databaseURL: "firebase_database_url",
+    projectId: "firebase_project_id",
+    storageBucket: "firebase_bucket",
+    messagingSenderId: "firebase_msg_sender_id"
+  }
+};
